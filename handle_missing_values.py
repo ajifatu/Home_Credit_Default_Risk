2a5c5d31ba22d26b80481145a7e3c28ec3e6f33a
NoForcing <jeanfabriceouffoue@gmail.com>
2025-02-17 21:55:08 +0000

    modification sur. gitignore et handle_missing_values.py(V2)

--- handle_missing_values.py
+++ handle_missing_values.py
@@ -46,6 +46,32 @@ def fetch_more_50_nan_columns(data:pd.DataFrame)->pd.DataFrame:
 
 
 
+def fetch_under_seuil_nan_columns(data:pd.DataFrame,seuil:float)->pd.DataFrame:
+
+    """
+     Selectionne et retourne  les colonnes  qui contiennent moins du pourcentage  
+     du "seuil' defini  de valeurs manquantes  d'un dataframe
+    """
+
+    # Sélectionner les colonnes avec des valeurs manquantes
+    colonnes_nan = data.columns[data.isnull().any()]
+
+    # Filtrer le DataFrame pour ne garder que les colonnes avec des nan
+    df_nan = data[colonnes_nan]
+
+    # Calcul du pourcentage de valeurs manquantes par colonne
+    pourcentage_nan = df_nan.isnull().mean() * 100
+
+    # Sélection des colonnes avec plus de 50% de valeurs manquantes
+    colonnes_a_supprimer = pourcentage_nan[pourcentage_nan < seuil].index
+
+    # Filtrer le DataFrame
+    df_filtre = df_nan[colonnes_a_supprimer]
+
+    return df_filtre
+
+
+
 # AFFICHER LE POURCENTAGE DE VALEURS MANQUANTES DANS UNE COLONNE SPECIFIQUE
 
 def nan_column_percent(data:pd.DataFrame,colname:str):
@@ -76,6 +102,7 @@ def nan_column_percent(data:pd.DataFrame,colname:str):
 
 # Variables quantitatives 
 
+# Idéale sans Valeurs Aberrantes 
 def impute_nan_with_mean(data:pd.DataFrame) -> None:
     """ 
     Impute les variables quantitatives manquantes  avec le la moyenne 
@@ -85,11 +112,11 @@ def impute_nan_with_mean(data:pd.DataFrame) -> None:
             data[col] = data[col].fillna(data[col].mean())
 
 
-
+# Idéale  avec Valeurs Aberrantes 
 def impute_nan_with_median(data:pd.DataFrame) -> None:
 
     """ 
-    Impute les variables quantitatives manquantes  avec le la mediane 
+    Impute les variables quantitatives manquantes  avec  la mediane 
     """
     for col in data.select_dtypes('number').columns:
         if not (-0.5 < data[col].skew() < 0.5):
@@ -137,3 +164,34 @@ def drop_more_50_nan_columns(data: pd.DataFrame) -> pd.DataFrame:
     data_cleaned = data.drop(columns=colonnes_a_supprimer)
 
     return data_cleaned
+
+
+def drop_more_threshold_nan_columns(data: pd.DataFrame,threshold:float) -> pd.DataFrame:
+    """
+    Supprime les colonnes qui contiennent plus de 50% de valeurs manquantes dans un DataFrame.
+    
+    :param data: DataFrame Pandas d'entrée
+    :return: DataFrame sans les colonnes ayant plus de 50% de NaN
+    """
+    # Calcul du pourcentage de valeurs manquantes par colonne
+    pourcentage_nan = data.isnull().mean() * 100
+
+    # Sélection des colonnes à supprimer (plus de 50% de NaN)
+    colonnes_a_supprimer = pourcentage_nan[pourcentage_nan > 50].index
+
+    # Supprimer ces colonnes du DataFrame
+    data_cleaned = data.drop(columns=colonnes_a_supprimer)
+
+    return data_cleaned
+
+
+
+# HANDLE MISSING VALUES ONE FONCTION 
+
+
+def handle_missing_values(data:pd.DataFrame) -> pd.DataFrame:
+    #drop_more_threshold_nan_columns(data,50)
+    impute_nan_with_mean(data)
+    impute_nan_with_median(data)
+    impute_nan_with_mode(data)
+    return data
