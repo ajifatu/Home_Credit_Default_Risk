078913d0f4ca48f2bde0362242cc9efe8315942e
NoForcing <jeanfabriceouffoue@gmail.com>
2025-02-21 01:22:34 +0000

    version 2

--- normalization_and_standardization.py
+++ normalization_and_standardization.py
@@ -104,11 +104,15 @@ def handle_outlier_winsor(data:pd.DataFrame) -> pd.DataFrame:
 
 #  FEATURE SCALING 
 
-def scaling(data:pd.DataFrame) -> pd.DataFrame:
+
+   
+
+
+def scaling_2(data:pd.DataFrame) -> pd.DataFrame:
      
     """
-     Cette fonction permet de mettres les colonnes numériques d'un dataset 
-     sur une meme echelle
+     Cette fonction permet de mettre les colonnes numériques d'un dataset 
+     sur une meme echelle, elle suppose qu'il n'y a pas de NaN
     """
 
     # selectionnes  uniquement les colonnes numériques sauf  des identifiants(SK_ID_CURR par eg)
@@ -116,53 +120,25 @@ def scaling(data:pd.DataFrame) -> pd.DataFrame:
 
         if col not in ["SK_ID_CURR","SK_BUREAU_ID","SK_ID_PREV"]:
                 # s'assurer que les valeurs aberrantes et NaN sont traitées aussi bien pour les colonnes symétriques qu'asymétriques
-                if not (-0.5 <= data[col].skew() <=0.5):
-                    if data.isna().sum().sum()== 0:
-                        # remplacer les valeurs aberrantes par Q1-1,5IQR ou  Q3-1,5IQR 
-                        data=replace_outliers_IQR(data)
-                        # mettre les données sur la  meme echelle  avec MinMaxScaler
-                        data_final=pd.DataFrame(MinMaxScaler().fit_transform(data),columns=data.columns)
-                        return data_final
-                    else:
-                        print("Le dataset contient des valeurs manquantes ")
-                    
-                else: 
-                    data=replace_outliers_Zscore(data)
-                    data_final=pd.DataFrame(StandardScaler().fit_transform(data),columns=data.columns)
-    
-    return data_final   
+                
+                if data[col].isna().sum().sum()== 0:
+                        if not (-0.5 <= data[col].skew() <=0.5):
+                            # remplacer les valeurs aberrantes par Q1-1,5IQR ou  Q3-1,5IQR 
+                            data[col]=replace_outliers_IQR(data[col])
+                            # mettre les données sur la  meme echelle  avec MinMaxScaler
+                            data[col]=pd.DataFrame(MinMaxScaler().fit_transform(data[[col]]),columns=data[[col]])
+                            return data
+                        else: 
+                            data[col]=replace_outliers_Zscore(data[col])
+                            data[col]=pd.DataFrame(StandardScaler().fit_transform(data[[col]]),columns=data[[col]])
+                else:
+                    col_na_number=data[col].isna().sum().sum()
+                    print(f"La colonne{col} contient {col_na_number} valeurs manquantes ")
+    return data   
 
 
 
-
-def scaling_with_na(data:pd.DataFrame) -> pd.DataFrame:
-     
-    """
-     Cette fonction permet de mettres les colonnes numériques d'un dataset 
-     sur une meme echelle
-    """
-
-    # selectionnes  uniquement les colonnes numériques sauf  des identifiants(SK_ID_CURR par eg)
-    for col in data.select_dtypes('number').columns:
-
-        if col not in ["SK_ID_CURR","SK_BUREAU_ID","SK_ID_PREV"]:
-                # s'assurer que les valeurs aberrantes et NaN sont traitées aussi bien pour les colonnes symétriques qu'asymétriques
-                if not (-0.5 <= data[col].skew() <=0.5):
-                        # remplacer les valeurs aberrantes par Q1-1,5IQR ou  Q3-1,5IQR 
-                        data=replace_outliers_IQR(data)
-                        # mettre les données sur la  meme echelle  avec MinMaxScaler
-                        data_final=pd.DataFrame(MinMaxScaler().fit_transform(data),columns=data.columns)
-                        return data_final
-                    
-                    
-                else: 
-                    data=replace_outliers_Zscore(data)
-                    data_final=pd.DataFrame(StandardScaler().fit_transform(data),columns=data.columns)
-    
-    return data_final  
-
-
-def scaling_with_na_2(data: pd.DataFrame) -> pd.DataFrame:
+def scaling_with_na(data: pd.DataFrame) -> pd.DataFrame:
     """
     Met toutes les colonnes numériques d'un dataset sur une même échelle,
     tout en gérant les valeurs aberrantes et les NaN.
@@ -188,7 +164,3 @@ def scaling_with_na_2(data: pd.DataFrame) -> pd.DataFrame:
             data[col] = standard_scaler.fit_transform(data[[col]])  # StandardScaler
             
     return data
-
-
-    # si les données sont asymétriques
-    # si les données sont symétriques
